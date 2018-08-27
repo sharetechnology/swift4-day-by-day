bb6343ee3f9481815a777e27724660b144672fed
shenzhaoliang <szl@uguoba.com>
2018-08-27 17:21:17 +0800

    2018年08月27日17:21:11 if demo

--- Swift4Demo/Swift4Demo/ViewController.swift
+++ Swift4Demo/Swift4Demo/ViewController.swift
@@ -15,18 +15,98 @@ class ViewController: UIViewController {
         
 //        substringTest()
         
-        stringTest()
+//        stringTest()
        
-        
+//        ifTest()
         
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func funcTest(){
+        
     }
-
+    
+    func ifTest() {
+        
+        let individualScores = [75, 43, 103, 87, 12]
+        var teamScore = 0
+        for score in individualScores {
+            if score > 50 {
+                teamScore += 3
+            } else {
+                teamScore += 1
+            }
+        }
+        print(teamScore)
+        
+        
+        let optionalString: String? = "Hello"
+        print(optionalString == nil)
+        
+        let optionalName: String? = "John Appleseed"
+        var greeting = "Hello!"
+        if let name = optionalName {
+            greeting = "Hello, \(name)"
+        }
+        
+        print(greeting)
+        
+        let nickName: String? = nil
+        let fullName: String = "John Appleseed"
+        let informalGreeting = "Hi \(nickName ?? fullName)"
+        
+        print(informalGreeting)
+        
+        let vegetable = "red pepper"
+        
+        //  MARK:删除 default 语句, 提示 “ Switch must be exhaustive ”，保证语句完整性
+        switch vegetable {
+        case "celery":
+            print("Add some raisins and make ants on a log.")
+        case "cucumber", "watercress":
+            print("That would make a good tea sandwich.")
+        case let x where x.hasSuffix("pepper"):
+            print("Is it a spicy \(x)?")
+        default:
+            print("Everything tastes good in soup.")
+        }
+        
+        
+        let interestingNumbers = [
+            "Prime": [2, 3, 5, 7, 11, 13],
+            "Fibonacci": [1, 1, 2, 3, 5, 8],
+            "Square": [1, 4, 9, 16, 25],
+            ]
+        var largest = 0
+        for (kind, numbers) in interestingNumbers {
+            for number in numbers {
+                if number > largest {
+                    largest = number
+                }
+            }
+        }
+        print(largest)
+        
+        
+        var n = 2
+        while n < 100 {
+            n *= 2
+        }
+        print(n)
+        
+        var m = 2
+        repeat {
+            m *= 2
+        } while m < 100
+        print(m)
+        
+        // MARK: 使用 ..< 约束的范围不包括上界，使用 ... 约束的范围包括上界
+        var total = 0
+        for i in 0..<4 {
+            total += i
+        }
+        print(total)
+    }
     
     func stringTest(){
         let apples = 3
