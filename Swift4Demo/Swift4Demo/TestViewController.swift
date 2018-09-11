75d53f1a079e2baee800c142d132f6f54616414c
shenzhaoliang <szl@uguoba.com>
2018-09-11 09:44:50 +0800

    2018年09月11日09:44:42 添加test

--- Swift4Demo/Swift4Demo/TestViewController.swift
+++ Swift4Demo/Swift4Demo/TestViewController.swift
@@ -24,7 +24,7 @@ class TestViewController: UIViewController{
         let tableView = UITableView.init(frame: self.view.frame, style: .grouped)
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
         return tableView
     }()
 }
@@ -35,17 +35,14 @@ extension TestViewController: UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 100
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell")
-        if cell == nil {
-            cell = UITableViewCell(style: .default, reuseIdentifier: "UITableViewCell")
-        }
-        cell?.textLabel?.text = String(indexPath.row)
-        cell?.detailTextLabel?.text = String(indexPath.row)
-        return cell!
+        let cell = TestCell.cell(tableView) as! TestCell
+        cell.contentLabel.text = String(indexPath.row)
+        print(indexPath)
+        return cell
     }
 }
 
@@ -53,6 +50,77 @@ extension TestViewController: UITableViewDelegate{
     
 }
 
+extension UITableViewCell{
+    class func cell(_ tableView: UITableView) -> UITableViewCell {
+        let className = NSStringFromClass(self).components(separatedBy: ".").last
+        var cell = tableView.dequeueReusableCell(withIdentifier: className!)
+        if cell == nil {
+            cell = self.init(style: .default, reuseIdentifier: className)
+            cell?.setupUI()
+            
+            print("------------")
+        }
+        return cell!
+    }
+    
+    @objc func setupUI(){
+        contentView.backgroundColor = .red
+    }
+}
+
+class TestCell: UITableViewCell {
+    override func setupUI() {
+        super.setupUI()
+        contentView.backgroundColor = .blue
+        contentView.addSubview(contentLabel)
+    }
+    
+    var contentLabel: UILabel = {
+        let contentLabel = UILabel()
+        contentLabel.frame = CGRect.init(x: 100, y: 10, width: 100, height: 20)
+        contentLabel.textColor = .red
+        return contentLabel
+    }()
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        print(subviews.count)
+    }
+}
+
+//class STCell: UITableViewCell {
+//    class func cell(_ tableView: UITableView) -> STCell {
+//        let className = NSStringFromClass(self).components(separatedBy: ".").last
+//        var cell = tableView.dequeueReusableCell(withIdentifier: className!)
+//        if cell == nil {
+//            cell = self.init(style: .default, reuseIdentifier: className)
+//        }
+//        return cell as! STCell
+//    }
+//
+//    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+//        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//        setupUI()
+//    }
+//
+//    //修饰词 final 表示 不可重写 可以将类或者类中的部分实现保护起来,从而避免子类破坏
+////    final func setupUI(){
+////        contentView.backgroundColor = .red
+////    }
+//
+//    func setupUI(){
+//         contentView.backgroundColor = .red
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//}
+//
+//class TestCell: STCell{
+//
+//}
 
 
 
